97471806908a623bfc4fc45fb3becf2ad534844d
Pritam Magdum <pritammagdum24@gmail.com>
2024-02-10 19:14:10 +0530

    Initial Commit

---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className="container">
+            <div className="container my-3">
+                <button className="btn btn-primary mx-2">-</button>
+                Add items to Cart
+                <button className="btn btn-primary mx-2">+</button>
+            </div>
+        </div>
+    )
+}
+
+export default Shop
